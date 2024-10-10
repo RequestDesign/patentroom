4f73c689e2186013296c5b5b07d0bb7f03cfa1b7
Tany <fen10wa@yandex.ru>
2024-10-10 12:55:31 +0300

    modal-cals

--- src/js/modal.js
+++ src/js/modal.js
@@ -9,3 +9,8 @@ $('[data-modal="modal-nl"]').on("click", () => {
   $("#modal-nl").addClass("modal-active");
   $("body").addClass("no-scroll");
 });
+
+$('[data-modal="modal-calc"]').on("click", () => {
+  $("#modal-calc").addClass("modal-active");
+  $("body").addClass("no-scroll");
+});
